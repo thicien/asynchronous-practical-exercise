e20b489de1c9b3654b2a621e0813f76e53b5e475
mugisha thicien <mugishathicien04@gmail.com>
2025-12-09 17:37:00 +0200

    working on exercises

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-console.log("Hello World");
 setTimeout(() => {
-    console.log("I am good");
-}, 2000)
-console.log("How was your weekend?");
+    console.log("one");
+}, 2000);
+setTimeout(() => {
+    console.log("two");
+}, 0);
+console.log("three")
