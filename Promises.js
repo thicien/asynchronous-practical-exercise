4a5adfe34fab3648bd9711c4b57881cd55d36f4f
mugisha thicien <mugishathicien04@gmail.com>
2025-12-18 14:40:27 +0200

    solve the async and await scenarios

--- Promises.js
+++ Promises.js
@@ -8,17 +8,3 @@ const promise = new Promise((resolve, reject) => {
     
 });
 
-function countPairs(arr) {
-    const frequency = {};
-    let pairs = 0;
-    for (let item of arr) {
-        frequency[item] = (frequency[item] || 0) + 1;
-    }
-    for (let key in frequency) {
-        pairs += Math.floor(frequency[key] / 2);
-    }
-    return pairs;
-}
-
-const items = ["apple", "banana", "apple", "orange", "banana", "apple"];
-console.log(countPairs(items));
