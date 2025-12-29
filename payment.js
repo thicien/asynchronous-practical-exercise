4d70380ca37b4298938e4d8529d55fd3de25e5d0
mugisha thicien <mugishathicien04@gmail.com>
2025-12-29 15:50:53 +0200

    The payment by using callbacks is now working

---
+++ payment.js
@@ -0,0 +1,10 @@
+function paymentProcess(amount, callback) {
+    console.log("Processing of payment: $" + amount);
+    setTimeout(() => {
+        console.log("Payment is successful");
+    }, 2000)
+}
+showPaymentConfirmation(message){
+    console.log("Payment Confirmation:" + message);
+}
+paymentProcess(1000, showPaymentConfirmation);
