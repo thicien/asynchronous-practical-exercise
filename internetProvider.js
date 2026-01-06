7f9754f03cc2875d8a57b08c6dc68f001feb15b3
mugisha thicien <mugishathicien04@gmail.com>
2026-01-06 17:20:57 +0200

    working on the internet provider

---
+++ internetProvider.js
@@ -0,0 +1,18 @@
+function isp1() {
+    return new Promise((resolve) =>
+        setTimeout(() => resolve("ISP 1 is connected"), 2000));
+}
+function isp2() {
+    return new Promise((resolve) =>
+        setTimeout(() => resolve("ISP 2 is connected"), 3000));
+}
+function isp3() {
+    return new Promise((resolve) =>
+        setTimeout(() => resolve("ISP 3 is connected"), 1000));
+}
+async function internet() {
+    const connection = await Promise.any([isp1(), isp2(), isp3()]);
+    console.log(connection);
+} catch(error) {
+    console.error("All IsPs are down");
+}
