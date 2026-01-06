ddd4861add11c13d386a2137eb810821e81a69e3
mugisha thicien <mugishathicien04@gmail.com>
2026-01-06 18:43:19 +0200

    The payment processed successfully

--- processPayment.js
+++ processPayment.js
@@ -4,7 +4,7 @@ function processPayment() {
 }
 function verifyAddress() { 
     return new Promise((resolve) =>
-        setTimeout(() => resolve("Address verified succcessfully"), 1000));
+        setTimeout(() => resolve("Address verified succcessfully!"), 1000));
 }
 function checkStock() {
     return new Promise((resolve) => 
@@ -12,7 +12,7 @@ function checkStock() {
 }
 async function checkOut() {
     try {
-        const results = await.Promise.all([processPayment(), verifyAddress(), checkStock()]);
+        const results = awaiPromise.all([processPayment(), verifyAddress(), checkStock()]);
         console.log("checkout completed:", results);
     } catch (error) {
         console.error("checkout failed:", error);
