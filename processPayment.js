c06ecf43f22760f265e4879b033a059a47710d45
mugisha thicien <mugishathicien04@gmail.com>
2026-01-06 18:47:23 +0200

    The payment processed successfully

--- processPayment.js
+++ processPayment.js
@@ -1,6 +1,6 @@
 function processPayment() {
     return new Promise((resolve) =>
-        setTimeout(() => resolve(" Payment processed successfully"), 2000));
+        setTimeout(() => resolve("Payment processed successfully"), 2000));
 }
 function verifyAddress() { 
     return new Promise((resolve) =>
