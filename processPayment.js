b3fd1daa5cdfe8ccd23ccb923e662cada2802cb7
mugisha thicien <mugishathicien04@gmail.com>
2026-01-06 18:42:08 +0200

    The payment processed successfully

--- processPayment.js
+++ processPayment.js
@@ -6,4 +6,17 @@ function verifyAddress() {
     return new Promise((resolve) =>
         setTimeout(() => resolve("Address verified succcessfully"), 1000));
 }
-function
+function checkStock() {
+    return new Promise((resolve) => 
+        setTimeout(() => resolve("Items are in the stock"), 1000));
+}
+async function checkOut() {
+    try {
+        const results = await.Promise.all([processPayment(), verifyAddress(), checkStock()]);
+        console.log("checkout completed:", results);
+    } catch (error) {
+        console.error("checkout failed:", error);
+
+    }
+}
+checkOut();
