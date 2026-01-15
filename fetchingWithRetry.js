783dd167c0ac63dad4a3e2d19a0f372e4cf2e1e3
mugisha thicien <mugishathicien04@gmail.com>
2026-01-15 11:34:03 +0200

    Checking how much the retrial

--- fetchingWithRetry.js
+++ fetchingWithRetry.js
@@ -1,22 +1,17 @@
 async function fetchWithRetry(url, retries = 3, delay = 1000) {
   try {
     const response = await fetch('https://api.github.com/users/${username}')
-
     if (!response.ok) {
       throw new Error(`HTTP error: ${response.status}`)
     }
-
       return await response.json();
 
   } catch (error) {
     if (retries === 0) {
       throw new Error("All retries failed")
     }
-
     console.log(`Retrying... (${retries} left)`)
-
     await new Promise(resolve => setTimeout(resolve, delay))
-
     return fetchWithRetry(url, retries - 1, delay)
   }
 }
