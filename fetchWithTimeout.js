bcf372b2177fdd32e1fde116c5be5344198bac76
mugisha thicien <mugishathicien04@gmail.com>
2026-02-02 13:40:53 +0200

    The logic for cancelling the fetch request has implemented

--- fetchWithTimeout.js
+++ fetchWithTimeout.js
@@ -1,10 +1,27 @@
 function fetchWithTimeout(URL, timeout) {
     const controller = new AbortController();
     const signal = controller.signal;
+    const timer = setTimeout(() => {
+        controller.abort();
+    }, timeout);
 
-    const fetchPromise = fetch(URL, { signal }).then(response => {
+    return fetchPromise = fetch(URL, { signal }).then(response => {
+        clearTimeout(timer);
         if (!response.ok) {
             throw new Error("Fetch failed");
+        } else {
+            return response.json();
         }
+    }).catch(error => {
+        if (error.name === "AbortError") {
+            throw new Error("Fetch request has timed out");
+        }
+        throw error;
     })
- }
+}
+fetchWithTimeout("https://jsonplaceholder.typicode.com/posts/1", 3000)
+    .then(data => {
+        console.log(data);
+    }).catch(error => {
+        console.error(error);
+    });
